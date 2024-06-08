6c20608ee9bdfc132cb3be54fd210fb7113d5fa6
jagingrich <jared.gingrich@gmail.com>
2024-06-07 20:06:15 -0400

    upload Clemson_CentennialBurOak LiDAR
    upload potree compression of Clemson University Champion Tree: Centennial Bur Oak LiDAR scan

---
+++ LiDAR_Projects/Clemson_CentennialBurOak/pointclouds/clemson_centennialburoak/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 224539295,
+    "boundingBox": {
+        "lx": -30.237000000000003,
+        "ly": -27.286,
+        "lz": 192.291,
+        "ux": 144.303,
+        "uy": 147.254,
+        "uz": 366.831
+    },
+    "tightBoundingBox": {
+        "lx": -30.237000000000003,
+        "ly": -27.286,
+        "lz": 192.291,
+        "ux": 144.303,
+        "uy": 143.799,
+        "uz": 224.76
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 1.5115607976913453,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
