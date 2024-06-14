fb2f42473c3f48e9515e98ae88654348cf8f7160
jagingrich <ccgt@clemson.edu>
2024-06-14 09:36:23 -0400

    Reupload CBO potree files

---
+++ LiDAR_Projects/Clemson_CentennialBurOak/pointclouds/clemson_centennialburoak/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 224539295,
+    "boundingBox": {
+        "lx": 441557.281,
+        "ly": 316550.719,
+        "lz": 189.703,
+        "ux": 441731.094,
+        "uy": 316724.53199999997,
+        "uz": 363.5159999999655
+    },
+    "tightBoundingBox": {
+        "lx": 441557.281,
+        "ly": 316550.719,
+        "lz": 189.703,
+        "ux": 441731.094,
+        "uy": 316717.844,
+        "uz": 222.17000000000003
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 1.5052647590637208,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
